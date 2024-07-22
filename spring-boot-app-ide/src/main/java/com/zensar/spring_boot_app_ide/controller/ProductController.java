5ede23a6abacc58f5fa77c0bec80cbb66879f6cc
Sharad Rajore <sharad.rajore@zensar.com>
2024-07-22 18:26:04 +0530

    added service layer support

--- spring-boot-app-ide/src/main/java/com/zensar/spring_boot_app_ide/controller/ProductController.java
+++ spring-boot-app-ide/src/main/java/com/zensar/spring_boot_app_ide/controller/ProductController.java
@@ -1,8 +1,8 @@
 package com.zensar.spring_boot_app_ide.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,18 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zensar.spring_boot_app_ide.dto.ProductDto;
+import com.zensar.spring_boot_app_ide.service.ProductService;
 
 @RestController
 public class ProductController {
 
-	List<ProductDto> products = new ArrayList<>();
-
-	public ProductController() {
-		products.add(new ProductDto(1, "HDD", 4500));
-		products.add(new ProductDto(2, "MAC", 45000));
-		products.add(new ProductDto(3, "IPad", 45000));
-
-	}
+	@Autowired
+	private ProductService productService;
 
 	// CR(Read)U D
 
@@ -29,26 +24,23 @@ public class ProductController {
 	// {RequestMethod.GET,RequestMethod.POST})
 
 	@GetMapping(value = "/product/{productId}")
-	public ProductDto getProduct(@PathVariable("productId") int productId) {
-		for(ProductDto product:products) {
-			if(product.getProductId()==productId)
-				return product;
-			
-		}
-		return null;
+	public ProductDto getProduct(@PathVariable int id) {
+		return productService.getProduct(id);
 	}
 
 	@GetMapping(value = "/product")
 	public List<ProductDto> getAllProducts() {
-		return products;
+		return productService.getAllProducts();
 	}
-	
-	
+
 	@PostMapping("/product")
 	public ProductDto insertProduct(@RequestBody ProductDto productDto) {
-		System.out.println("*******************************"+productDto);
-		products.add(productDto);
+		productService.insertProduct(productDto);
 		return productDto;
 	}
+
+	// update
+
+	// delete
 
 }
